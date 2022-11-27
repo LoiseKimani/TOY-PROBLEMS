2e198b5a8de1cdaf6dfe5ff8dac60496f830435f
Loise <loise.kimani@student.moringaschool.com>
2022-11-27 18:03:08 +0300

    alert to enter result

--- Student Grade Generator.js
+++ Student Grade Generator.js
@@ -3,7 +3,7 @@ let result =parseInt(prompt("Enter student result:"));
 function studentGrade() {
   
     if(result > 100) {
-        alert("enter valid result")   alert("enter valid result")
+        alert("enter valid result")   
     }else if (result>= 80) {
        return "A"
     }else if (result>=60 ) {
